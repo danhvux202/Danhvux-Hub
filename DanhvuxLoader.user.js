f0480d84cfde81e71b995b59469fca4e646bfaa5
danhvux202 <vudanh622@gmail.com>
2026-03-10 13:13:17 +0700

    Update DanhvuxLoader.user.js

--- DanhvuxLoader.user.js
+++ DanhvuxLoader.user.js
@@ -1,28 +1,34 @@
 // ==UserScript==
 // @name         Danhvux Hub Loader
 // @namespace    https://github.com/danhvux
-// @version      1.0
+// @version      1.0.0
 // @description  Loader for Danhvux Hub
 // @author       Danhvux
 // @match        https://*.k12online.vn/*
 // @run-at       document-start
 // @grant        GM_xmlhttpRequest
 // @connect      raw.githubusercontent.com
+// @updateURL    https://raw.githubusercontent.com/danhvux202/Danhvux-Hub/main/DanhvuxLoader.user.js
+// @downloadURL  https://raw.githubusercontent.com/danhvux202/Danhvux-Hub/main/DanhvuxLoader.user.js
 // ==/UserScript==
 
-(function(){
+(function () {
+'use strict';
 
-const url = "https://raw.githubusercontent.com/danhvux202/Danhvux-Hub/main/main.js?t=" + Date.now();
+const SCRIPT_URL =
+"https://raw.githubusercontent.com/danhvux202/Danhvux-Hub/main/main.js?t=" + Date.now();
 
 GM_xmlhttpRequest({
 method:"GET",
-url:url,
-onload:function(r){
+url:SCRIPT_URL,
+onload:function(res){
+
 try{
-eval(r.responseText);
+eval(res.responseText);
 }catch(e){
-console.error("Hub load error",e);
+console.error("Hub load error:", e);
 }
+
 }
 });
 
