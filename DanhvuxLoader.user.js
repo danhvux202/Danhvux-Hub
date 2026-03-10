518459adee974b05ddd0b697e7d71ea7e71cb14c
danhvux202 <vudanh622@gmail.com>
2026-03-10 20:25:28 +0700

    Modify DanhvuxLoader.user.js metadata and execution timing
    Updated user script metadata and changed run-at timing.

--- DanhvuxLoader.user.js
+++ DanhvuxLoader.user.js
@@ -1,33 +1,22 @@
 // ==UserScript==
 // @name         Danhvux Hub Loader
-// @namespace    https://github.com/danhvux
-// @version      1.0.0
-// @description  Loader for Danhvux Hub
-// @author       Danhvux
 // @match        https://*.k12online.vn/*
-// @run-at       document-start
+// @run-at       document-end
 // @grant        GM_xmlhttpRequest
 // @connect      raw.githubusercontent.com
-// @updateURL    https://raw.githubusercontent.com/danhvux202/Danhvux-Hub/main/DanhvuxLoader.user.js
-// @downloadURL  https://raw.githubusercontent.com/danhvux202/Danhvux-Hub/main/DanhvuxLoader.user.js
 // ==/UserScript==
 
-(function () {
-'use strict';
+(function(){
 
-const SCRIPT_URL =
-"https://raw.githubusercontent.com/danhvux202/Danhvux-Hub/main/main.js?t=" + Date.now();
+const url =
+"https://raw.githubusercontent.com/danhvux202/Danhvux-Hub/main/main.js?"+Date.now();
 
 GM_xmlhttpRequest({
 method:"GET",
-url:SCRIPT_URL,
-onload:function(res){
+url:url,
+onload:r=>{
 
-try{
-eval(res.responseText);
-}catch(e){
-console.error("Hub load error:", e);
-}
+new Function(r.responseText)();
 
 }
 });
