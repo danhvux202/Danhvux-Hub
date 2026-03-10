f65dc873b9efe7f12d5d2a8c069673b126ae1cdf
danhvux202 <vudanh622@gmail.com>
2026-03-10 20:52:05 +0700

    Remove auto login feature from main.js
    Removed auto login functionality and related code.

--- main.js
+++ main.js
@@ -236,56 +236,6 @@ if(v) v.playbackRate=currentSpeed;
 };
 }
 
-//////////////////////////////////////////////////
-// AUTO LOGIN
-//////////////////////////////////////////////////
-
-const loginBtn=document.getElementById("btnForceLogin");
-
-if(loginBtn){
-
-loginBtn.onclick=()=>{
-
-const inputs=document.querySelectorAll("input");
-
-let uInp,pInp;
-
-inputs.forEach(inp=>{
-
-const type=inp.type?.toLowerCase();
-
-if(type==="password") pInp=inp;
-
-if(type==="text"||type==="email"){
-if(!uInp) uInp=inp;
-}
-
-});
-
-if(uInp&&pInp){
-
-deepFill(uInp,accounts[0].u);
-
-setTimeout(()=>{
-
-deepFill(pInp,accounts[0].p);
-
-setTimeout(()=>{
-document.querySelector("button[type=submit]")?.click();
-},400);
-
-},400);
-
-}else{
-
-alert("Không tìm thấy form login");
-
-}
-
-};
-
-}
-
 };
 
 //////////////////////////////////////////////////
@@ -302,8 +252,6 @@ if(v){
 
 v.muted=true;
 
-if(currentSpeed>20) currentSpeed=20;
-
 if(v.playbackRate!==currentSpeed)
 v.playbackRate=currentSpeed;
 
@@ -312,95 +260,10 @@ document.querySelector(".btn-next,.next-lesson,.next-item")?.click();
 
 }
 
-const jBox=document.getElementById("jumpBox");
-if(!jBox) return;
-
-let missing=[];
-
-document
-.querySelectorAll(".lesson-item,.fancytree-node,tr,li")
-.forEach(el=>{
-
-let t=el.innerText;
-
-if(t.includes("%")){
-
-let p=parseInt(
-t.match(/(\d+)%/)?.[1]||"100"
-);
-
-if(p<100){
-
-let link=
-el.querySelector("a")?.href||
-el.getAttribute("href");
-
-let title=t.split("\n")[0]
-.trim()
-.substring(0,20);
-
-if(title.length>2)
-missing.push({title,link,el,p});
-
-}
-
-}
-
-});
-
-if(missing.length){
-
-const unique=
-Array.from(
-new Map(missing.map(x=>[x.title,x]))
-.values()
-);
-
-jBox.innerHTML=unique.map((item,i)=>`
-<div class="force-jump"
-data-idx="${i}"
-style="
-padding:6px;
-margin-bottom:4px;
-background:#313244;
-border-radius:4px;
-cursor:pointer;
-color:#f5e0dc;
-border-left:3px solid #f38ba8;">
-⚡ ${item.title} (${item.p}%)
-</div>
-`).join("");
-
-document.querySelectorAll(".force-jump")
-.forEach(btn=>{
-
-btn.onclick=function(){
-
-const target=
-unique[this.getAttribute("data-idx")];
-
-if(target.link&&target.link.startsWith("http"))
-window.location.href=target.link;
-
-else{
-target.el.scrollIntoView();
-target.el.click();
-}
-
-};
-
-});
-
-}else{
-
-jBox.innerHTML="🎉 Đã xong 100%!";
-
-}
-
 };
 
 //////////////////////////////////////////////////
-// START SYSTEM
+// START
 //////////////////////////////////////////////////
 
 function start(){
